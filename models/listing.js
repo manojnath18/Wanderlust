33322f0e24083589072d1cf229b9c62494f706d7
Manoj Nath <161053926+manojnath18@users.noreply.github.com>
2026-09-12 01:10:57 +0545

    configured cloudinary for image storage

--- models/listing.js
+++ models/listing.js
@@ -10,10 +10,8 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
-        type: String,
-        default: "https://plus.unsplash.com/premium_photo-1710965560034-778eedc929ff?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmVhdXRpZnVsfGVufDB8fDB8fHww",
-        set: (v)=> v=== ""? "https://plus.unsplash.com/premium_photo-1710965560034-778eedc929ff?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmVhdXRpZnVsfGVufDB8fDB8fHww"
-        : v, 
+        url: String,
+        filename: String, 
     },
     price: Number,
     location: String,
